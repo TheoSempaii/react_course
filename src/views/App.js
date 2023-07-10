99ca0f76896c32a0e5c88a33602513d345c0d005
Emanuel Guzman <emanuel250gameryt@outlook.com>
2023-07-10 16:47:07 -0300

    Working with components

--- src/views/App.js
+++ src/views/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import Navbar from "../components/Navbar";
 
 function App() {
   const likes = 110;
   const arr = [0, 1, 2, 3, 4, 5]
   return (
     <div className="App">
+      <Navbar />
       <h1>test</h1>
       <p>{likes}</p>
       <p>{arr}</p>
