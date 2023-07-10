a8b3b44ff62240f222f187ed8f48ee8635f4ddd0
Emanuel Guzman <emanuel250gameryt@outlook.com>
2023-07-10 15:00:43 -0300

    react cap#4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 
 function App() {
+  const likes = 110;
+  const arr = [0, 1, 2, 3, 4, 5]
   return (
     <div className="App">
-      <h1>a</h1>
-      
+      <h1>test</h1>
+      <p>{likes}</p>
+      <p>{arr}</p>
     </div>
   );
 }
