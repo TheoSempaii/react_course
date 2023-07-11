9f6dde15c1b2537adfc5fef65f33c6c129fc016c
Emanuel Guzman <emanuel250gameryt@outlook.com>
2023-07-11 16:38:41 -0300

    reusing components

---
+++ src/components/Products.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const Products = (props = {
+  filter: (p) => p
+}) => {
+
+  let [products, setProducts] = useState([{
+    id: 1,
+    name: "Product 1",
+    price: 1000,
+    description: "This is a product 1",
+  }, {
+    id: 2,
+    name: "Product 2",
+    price: 1000,
+    description: "This is a product 2",
+  }])
+
+  products = products.filter(props.filter)
+
+  return (
+    <div className="product-container">
+      {products.map((product) => (
+        <div className="product-box" key={product.id}>
+          <h1>{product.name}</h1>
+          <p>{product.description}</p>
+          <p className="price">{product.price}</p>
+
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Products;
